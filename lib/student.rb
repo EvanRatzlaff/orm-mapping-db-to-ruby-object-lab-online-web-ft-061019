57e934513f0a8e5eda9fd50ba422e71813feef07
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-07-24 00:09:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -114,8 +114,7 @@ end
  
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end
-  end 
+    end.first
   def self.all_students_in_grade_X(x)
     sql = <<-SQL
     SELECT *
